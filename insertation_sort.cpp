a620b11e3ca1e74aaff434df6137cabbbcb924d6
mimyutt <mutiakhofifaturr@gmail.com>
2026-03-12 14:49:48 +0700

    add void display

--- insertation_sort.cpp
+++ insertation_sort.cpp
@@ -55,3 +55,16 @@ void insertionsort()
         }
     }
 }
+void display()
+{
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElemen Array" << endl;
+    cout << "\n=================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
